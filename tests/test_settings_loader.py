3e7784b66c944e61a508f90817e83b35cf5f9df2
Florin Pop <florin@ae.studio>
2023-05-12 10:31:50 +0200

    Add more tests for the migrations

--- tests/test_settings_loader.py
+++ tests/test_settings_loader.py
@@ -1,6 +1,8 @@
 """Test settings_loader.py module."""
 import os.path
 from pathlib import Path
+import sys
+import types
 from unittest.mock import call
 from unittest.mock import mock_open
 from unittest.mock import patch
@@ -51,7 +53,7 @@ class TestSettingsLoader:
         )
 
     @patch("pathlib.Path.exists")
-    def test_settings_loader_raises_exception_when_default_settings_file_is_not_found(
+    def test_raises_exception_when_default_settings_file_is_not_found(
         self, mock_path_exists, mock_open
     ):
         """Test settings_loader returns settings from default file."""
@@ -62,12 +64,45 @@ class TestSettingsLoader:
                 settings_file, "test_settings.yaml", _SettingsModel
             )
 
-    def test_settings_loader_raises_migration_error_for_wrong_version(self, mock_open):
-        """Test calling the settings_loader with an unexpected settings version."""
+    def test_raises_migration_error_for_nonexisting_migration(self, mock_open):
+        """Test calling the settings_loader with an unmigrated settings file."""
         settings_file = Path("some/path/on/disk")
-        with pytest.raises(SettingsMigrationError):
+        with pytest.raises(SettingsMigrationError) as e:
             # Settings model expects a newer version than 1.0.0
             # but no migration is found for the file `test_settings.yaml`
             settings_loader.get_script_settings(
                 settings_file, "test_settings.yaml", Settings
             )
+        assert str(e.value) == str(
+            "No migrations found for this settings file. " "Unable to migrate settings."
+        )
+
+    def test_raises_migration_error_for_nonexisting_module(self, mock_open):
+        """Test settings_loader with a settings file and an unknown migration module."""
+        settings_file = Path("some/path/on/disk")
+        with pytest.raises(SettingsMigrationError) as e:
+            # Settings model expects a newer version than 1.0.0
+            # but the provided settings module is not found
+            settings_loader.get_script_settings(
+                settings_file, "test_settings.yaml", Settings, module_name="nonexisting"
+            )
+        assert str(e.value) == "Migration module not found. Unable to migrate settings."
+
+    def test_raises_migration_error_for_wrong_existing_module(self, mock_open):
+        """Test settings_loader with a settings file and an bad migration module.
+
+        The migration module exists but does not have a MIGRATIONS attribute.
+        """
+        settings_file = Path("some/path/on/disk")
+        with pytest.raises(SettingsMigrationError) as e:
+            # Settings model expects a newer version than 1.0.0
+            # but the provided settings module does not have a MIGRATIONS attribute
+            migrations_module = types.ModuleType("migrations", "The migrations module")
+            sys.modules["tests.config.migrations"] = migrations_module
+            settings_loader.get_script_settings(
+                settings_file, "test_settings.yaml", Settings, module_name="tests"
+            )
+        assert str(e.value) == str(
+            "Migration module does not have a MIGRATIONS attribute. "
+            "Unable to migrate settings."
+        )
