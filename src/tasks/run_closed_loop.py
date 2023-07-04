cccc9ec94aa75c1b48d701e71b2c5e55c18cad6a
Antonio Eudes <aeudes@gmail.com>
2023-07-04 10:07:21 -0300

    Fix docstring style

--- src/tasks/run_closed_loop.py
+++ src/tasks/run_closed_loop.py
@@ -35,9 +35,12 @@ def _create_pipe_file(pipe_name: str) -> tuple[str, str]:
 def _wait_for_pipe_message(pipe_path: str, pipe_message: str) -> bool:
     """Wait for a pipe message in a pipe file.
 
-    :param pipe_path: Path to the pipe file.
-    :param pipe_message:  Message to wait for.
-    :returns: True if the message was found, False if the process was interrupted.
+    Args:
+        pipe_path: Path to the pipe file.
+        pipe_message:  Message to wait for.
+
+    Returns:
+        True if the message was found, False if the process was interrupted.
     """
     try:
         with open(pipe_path, "r") as center_out_reach_pipe:
